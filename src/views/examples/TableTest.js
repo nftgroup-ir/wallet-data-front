940ba5e4370826950a1bbb52ee9540e86f8c0b49
mhmmdhajizade <mohammad.hajizadehk@gmail.com>
2021-11-28 13:36:13 +0330

    mainTable server data added

--- src/views/examples/TableTest.js
+++ src/views/examples/TableTest.js
@@ -1,4 +1,4 @@
-import React, { forwardRef , useEffect , useState } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
 import AddBox from '@material-ui/icons/AddBox';
 import ArrowDownward from '@material-ui/icons/ArrowDownward';
 import Check from '@material-ui/icons/Check';
@@ -19,126 +19,96 @@ import Header from 'components/Headers/Header';
 
 
 const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
 };
 
 function TableTest() {
 
-  const [csvItems, setcsvItems] = useState([""])
+  const [csvItems, setcsvItems] = useState([])
 
   useEffect(() => {
-    fetch('http://65.108.59.117:7001api/csv/' ,{
-      method:'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Token ' + sessionStorage.getItem('token')
-      },
-    })
-      .then(res=> res.json())
-      .then(data=> {
-        setcsvItems(data)
-        console.log(csvItems)
-        console.log(data)
+    async function getData() {
+      await fetch('http://65.108.59.117:7001/api/csv/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Token ' + sessionStorage.getItem('token')
+        },
       })
+        .then(res => res.json())
+        .then(data => {
+          setcsvItems(data.reverse())
+          console.log(data)
+        })
+    }
+    getData()
+    
   }, []);
+  console.log(csvItems)
+  const dataTaken = csvItems
+  console.log(dataTaken)
 
-    return (
+  return (
 
-        <div className="align-items-center">
-            <Header />
-            <MaterialTable
-                style={{ maxWidth: '90%', display: 'block', margin: '-50px auto 0px auto' }}
-                title="Basic Export Preview"
-                columns={[
-                    { title: 'Name', field: 'name' },
-                    { title: 'Address', field: 'Address' },
-                    { title: 'Point', field: 'Point', type: 'numeric' },
-                    // {
-                    //     title: 'Birth Place',
-                    //     field: 'birthCity',
-                    //     lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-                    // },
-                    { title: 'Transactions', field: 'Transactions', type: 'numeric' },
-                    { title: 'NFT', field: 'NFT', type: 'numeric' },
-                    { title: 'Balance', field: 'Balance', type: 'numeric' },
-                ]}
-                data={[{
-                    Address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
-                    name: "hani@gmail.com",
-                    Point: 2,
-                    Transactions: 158,
-                    NFT: 0,
-                    Balance: 2115743901717
-                  },
-                 
-                  {
-                    Address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
-                    name: "hani@gmail.com",
-                    Point: 2,
-                    Transactions: 158,
-                    NFT: 0,
-                    Balance: 2115743901717                  
-                  },
-                  {
-                    Address: "0x6b829251366c43BB3621BE6eeFC41CdbD514380c",
-                    name: "mohammad@gmail.com",
-                    Point: 3,
-                    Transactions: 0,
-                    NFT: 0,
-                    Balance: 0
-                  },
-                  {
-                    Address: "0x6b829251366c43BB3621BE6eeFC41CdbD514380c",
-                    name: "mohammad@gmail.com",
-                    Point: 3,
-                    Transactions: 0,
-                    NFT: 0,
-                    Balance: 0
-                  },
-                  {
-                    Address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
-                    name: "hani@gmail.com",
-                    Point: 2,
-                    Transactions: 158,
-                    NFT: 0,
-                    Balance: 2115743901717
-                  },
-                  {
-                    Address: "0x6b829251366c43BB3621BE6eeFC41CdbD514380c",
-                    name: "mohammad@gmail.com",
-                    Point: 3,
-                    Transactions: 0,
-                    NFT: 0,
-                    Balance: 0
-                  },
-                  ]}
-                options={{
-                    exportButton: true,
-                    filtering: true,
-                    search: true
-                }}
+    <div className="align-items-center">
+      <Header />
+      <MaterialTable
+        style={{ maxWidth: '90%', display: 'block', margin: '-50px auto 0px auto' }}
+        title="Basic Export Preview"
+        columns={[
+          { title: 'Name', field: 'address' },
+          { title: 'Address', field: 'email' },
+          { title: 'Point', field: 'points' },
+          { title: 'Transactions', field: 'transactions', type: 'numeric' },
+          { title: 'NFT', field: 'NFT', type: 'numeric' },
+          { title: 'Balance', field: 'balancedata[0].balance', type: 'numeric' },
+        ]}
+        // data={[{
+        //   address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+        //   email: "hani@gmail.com",
+        //   points: 2,
+        //   transactions: 158,
+        //   NFT: 0,
+        //   balance: 2115743901717
+        //   },
 
-                icons={tableIcons}
+        //   ]}
+        data={query =>
+          new Promise((resolve, reject) => {
+            resolve({
+              data: dataTaken,
+              page: 10,
+              totalCount: 646,
+            })
+          })
+        }
+        options={{
+          exportButton: true,
+          filtering: true,
+          search: true
+        }}
 
-            />
-        </div>
-    )
+        icons={tableIcons}
+
+      />
+    </div>
+  )
 }
 
 export default TableTest
